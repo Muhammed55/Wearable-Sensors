91db183743fb65920ff65a331b5ce7e1f4aa5b5a
Muhammed55 <24507268+Muhammed55@users.noreply.github.com>
2022-11-24 17:51:43 +0100

    Add files via upload

---
+++ HMOS/entry/src/main/java/com/mo55/test/wearenginehos/MyApplication.java
@@ -0,0 +1,10 @@
+package com.mo55.test.wearenginehos;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
